2c741e7934652dc236c8171ffa1633e3783a859f
Xavier Garrido <xavier.garrido@gmail.com>
2014-07-06 11:31:40 +0200

    fix

--- zsh-aggregator.plugin.zsh
+++ zsh-aggregator.plugin.zsh
@@ -696,8 +696,7 @@ function __aggregator_set_bayeux
         -DCMAKE_BUILD_TYPE:STRING=Release                \
         -DCMAKE_INSTALL_PREFIX=${aggregator_install_dir} \
         -DCMAKE_PREFIX_PATH=${cadfael_install_dir}       \
-        -DBayeux_WITH_GEANT4=ON                          \
-        -DBayeux_FORCE_CXX_ALL_WARNINGS=OFF
+        -DBayeux_WITH_GEANT4=ON
     "
     if ${with_warning}; then
         aggregator_options+="-DBayeux_FORCE_CXX_ALL_WARNINGS=ON "
